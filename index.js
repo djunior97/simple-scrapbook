eca868d8bb740501c7f92960be128624a0f4bbba
dirceuAntunes <dirceu.antunes@garupa.co>
2020-07-02 21:20:15 -0300

    create add new scrap routine

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 let titleInput = document.getElementById("messageTitle");
 let messageInput = document.getElementById("messageBody");
 let addScrapBtn = document.getElementById("addButton");
+let scrapsField = document.getElementById("scrapsField");
+
+let scraps = [];
+
+function renderScraps() {
+  scrapsField.innerHTML = "";
+
+  for (const scrap of scraps) {
+    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+  }
+}
+
+function addNewScrap() {
+  let title = titleInput.value;
+  let message = messageInput.value;
+
+  titleInput.value = "";
+  messageInput.value = "";
+
+  scraps.push({ title, message });
+
+  renderScraps();
+}
+
+function createScrapCard(title, message) {
+  return `
+  <div class="message-cards card text-white bg-dark m-2 col-3">
+    <div class="card-header font-weight-bold">${title}</div>
+    <div class="card-body">
+      <p class="card-text">
+        ${message}
+      </p>
+    </div>
+  </div>
+  `;
+}
+
+renderScraps();
+addScrapBtn.onclick = addNewScrap;
