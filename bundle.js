a6f93ad23cc8182e4c3c63c62a61384d7437b719
dirceuAntunes <dirceu.antunes@garupa.co>
2020-07-16 20:35:18 -0300

    create class TaskList and constructor methods

--- bundle.js
+++ bundle.js
@@ -1,3 +1,15 @@
 "use strict";
 
-alert("teste");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TaskList = function TaskList() {
+  _classCallCheck(this, TaskList);
+
+  this.titleInput = document.getElementById("messageTitle");
+  this.messageInput = document.getElementById("messageBody");
+  this.addBtn = document.getElementById("addButton");
+  this.scrapsField = document.getElementById("scrapsField");
+  this.scraps = [];
+};
+
+new TaskList();
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("teste");
+class TaskList {
+  constructor() {
+    this.titleInput = document.getElementById("messageTitle");
+    this.messageInput = document.getElementById("messageBody");
+    this.addBtn = document.getElementById("addButton");
+    this.scrapsField = document.getElementById("scrapsField");
+
+    this.scraps = [];
+  }
+}
+
+new TaskList();
