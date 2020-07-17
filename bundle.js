8a700cc0d5d174d1aa20e2f39b1807de5095c9d2
dirceuAntunes <dirceu.antunes@garupa.co>
2020-07-16 21:10:33 -0300

    add create new scrap functionality

--- bundle.js
+++ bundle.js
@@ -1,15 +1,78 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var TaskList = function TaskList() {
-  _classCallCheck(this, TaskList);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  this.titleInput = document.getElementById("messageTitle");
-  this.messageInput = document.getElementById("messageBody");
-  this.addBtn = document.getElementById("addButton");
-  this.scrapsField = document.getElementById("scrapsField");
-  this.scraps = [];
-};
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var TaskList = /*#__PURE__*/function () {
+  function TaskList() {
+    _classCallCheck(this, TaskList);
+
+    this.titleInput = document.getElementById("messageTitle");
+    this.messageInput = document.getElementById("messageBody");
+    this.addBtn = document.getElementById("addButton");
+    this.scrapsField = document.getElementById("scrapsField");
+    this.scraps = [];
+    this.registerAddScrapBtnEvent();
+  }
+
+  _createClass(TaskList, [{
+    key: "registerAddScrapBtnEvent",
+    value: function registerAddScrapBtnEvent() {
+      var _this = this;
+
+      this.addBtn.onclick = function () {
+        return _this.addNewScrap();
+      };
+    }
+  }, {
+    key: "renderScraps",
+    value: function renderScraps() {
+      this.scrapsField.innerHTML = "";
+
+      var _iterator = _createForOfIteratorHelper(this.scraps),
+          _step;
+
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var scrap = _step.value;
+          this.scrapsField.innerHTML += this.createScrapCard(scrap.title, scrap.message);
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+    }
+  }, {
+    key: "addNewScrap",
+    value: function addNewScrap() {
+      var title = this.titleInput.value;
+      var message = this.messageInput.value;
+      this.titleInput.value = "";
+      this.messageInput.value = "";
+      this.scraps.push({
+        title: title,
+        message: message
+      });
+      this.renderScraps();
+    }
+  }, {
+    key: "createScrapCard",
+    value: function createScrapCard(title, message) {
+      return "\n    <div class=\"message-cards card text-white bg-dark m-2 col-3\">\n      <div class=\"card-header font-weight-bold\">".concat(title, "</div>\n      <div class=\"card-body\">\n        <p class=\"card-text\">\n          ").concat(message, "\n        </p>\n      </div>\n      <div class=\"w-100 d-flex justify-content-end pr-2 pb-2\">\n        <button class=\"btn btn-danger mr-1\">Deletar</button>\n        <button class=\"btn btn-info\">Editar</button>\n      </div>\n    </div>\n    ");
+    }
+  }]);
+
+  return TaskList;
+}();
 
 new TaskList();
--- main.js
+++ main.js
@@ -6,6 +6,52 @@ class TaskList {
     this.scrapsField = document.getElementById("scrapsField");
 
     this.scraps = [];
+
+    this.registerAddScrapBtnEvent();
+  }
+
+  registerAddScrapBtnEvent() {
+    this.addBtn.onclick = () => this.addNewScrap();
+  }
+
+  renderScraps() {
+    this.scrapsField.innerHTML = "";
+
+    for (const scrap of this.scraps) {
+      this.scrapsField.innerHTML += this.createScrapCard(
+        scrap.title,
+        scrap.message
+      );
+    }
+  }
+
+  addNewScrap() {
+    let title = this.titleInput.value;
+    let message = this.messageInput.value;
+
+    this.titleInput.value = "";
+    this.messageInput.value = "";
+
+    this.scraps.push({ title, message });
+
+    this.renderScraps();
+  }
+
+  createScrapCard(title, message) {
+    return `
+    <div class="message-cards card text-white bg-dark m-2 col-3">
+      <div class="card-header font-weight-bold">${title}</div>
+      <div class="card-body">
+        <p class="card-text">
+          ${message}
+        </p>
+      </div>
+      <div class="w-100 d-flex justify-content-end pr-2 pb-2">
+        <button class="btn btn-danger mr-1">Deletar</button>
+        <button class="btn btn-info">Editar</button>
+      </div>
+    </div>
+    `;
   }
 }
 
